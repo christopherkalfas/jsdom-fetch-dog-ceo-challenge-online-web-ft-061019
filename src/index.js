c80b9c8b71995c7091f19fb1ee7b40f2a162397b
github username <github email address>
2019-09-23 16:18:02 -0400

    almost got the last one need to take a break

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+let filter = null
 
 
 document.addEventListener("DOMContentLoaded", fetchDog)
 
 document.addEventListener("DOMContentLoaded", fetchAllDogsBreeds)
+
+document.addEventListener("change", filterBreed )
 
 function fetchDog() {
     fetch(imgUrl)
@@ -29,15 +32,37 @@ function fetchAllDogsBreeds(){
 }
 
 function listBreed(breeds){
-    let dogBreeds = document.getElementById("dog-breeds")
+    let dogBreeds = document.querySelector('[id="dog-breeds"]')
         for(breed in breeds) {
+            if(!filter || breed.startsWith(filter)){
             let newBreedElement = document.createElement("li")
             newBreedElement.innerText = breed 
             dogBreeds.appendChild(newBreedElement)
             newBreedElement.addEventListener("click", colorChangeHandler)
+            }
         }
 }
 
 function colorChangeHandler(e) {
     e.target.style.color = 'dodgerblue'
+}
+
+function filterBreed(){
+    let dogBreeds = document.querySelector('[id="dog-breeds"]')
+    let breedList = dogBreeds.querySelectorAll("li")
+}
+
+function filterHandler(event) {
+    let dogBreeds = document.querySelector('[id="dog-breeds"]')
+    let breedList = dogBreeds.querySelectorAll("li")
+
+    breedList.forEach(breed => {
+        if (event.target.value === "") {
+            breed.style.display = "block"
+        } else if (breed.children[0].id[0] != event.target.value) {
+            breed.style.display = "none"
+        } else {
+            breed.style.display = "block"
+        }
+    })
 }
