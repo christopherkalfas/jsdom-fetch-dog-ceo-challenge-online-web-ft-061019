3d13b1faa1b9c6d3e6c69c1d729ec0a495829963
github username <github email address>
2019-09-23 14:49:39 -0400

    challenge 1, DONE

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener("DOMContentLoaded", fetchDog)
+
+function fetchDog() {
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(json => dogImg(json.message))
+}
+
+function dogImg(imgArray){
+    let dogContainer = document.getElementById("dog-image-container")
+ 
+    imgArray.forEach(dogUrl => {
+        dogContainer.innerHTML += `<img src="${dogUrl}">`
+    })
+}
